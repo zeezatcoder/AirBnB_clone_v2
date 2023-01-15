dd9cb62ec824b04030dd5baed31d3f0721e88da0
zeezatcoder <azeezatmaja@gmail.com>
2023-01-15 13:44:58 -0800

    task5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display_hello():
+    """a function to print hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def desplay_hbnb():
+    """function to print hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_cText(text):
+    """print C with passed variable"""
+    text = text.replace("_", " ")
+    return "C %s" % (text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_pythonText(text='is cool'):
+    """ a function called with /python/<text> route """
+    if text != 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % (text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_if_int(n):
+    return '%d is a number' % (n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def template_render(n):
+    """ render template if number is an integer"""
+    return render_template('5-number.html', num=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
