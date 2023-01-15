058ebb887086bb115e90ba322f68618c3cb1d477
zeezatcoder <azeezatmaja@gmail.com>
2023-01-15 14:15:44 -0800

    task7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that runs a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import 
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """get all states"""
+    states = storage.all("State")
+    return render_template('7-states_list.html',
+                           Table="States", states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
