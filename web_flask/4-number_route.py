b41e3f885bb543547df2cd65655554ed48c4699a
zeezatcoder <azeezatmaja@gmail.com>
2023-01-15 13:28:25 -0800

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display_hello():
+    """a function to prints hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def desplay_hbnb():
+    """function to print hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_cText(text):
+    """print C with passed variable"""
+    text = text.replace("_", " ")
+    return "C %s" % (text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_pythonText(text='is cool'):
+    """ Function called with /python/<text> route """
+    if text != 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % (text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_if_int(n):
+    return '%d is a number' % (n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
